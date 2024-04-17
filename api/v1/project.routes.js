927969d4058f11edbbf61678a2fbfc19a3609e17
faiyaz032 <faiyazrahman03@gmail.com>
2024-04-17 21:50:23 +0600

    fix modules issue

--- api/v1/project.routes.js
+++ api/v1/project.routes.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const projectController = require("../../controllers/project.controller");
+const express = require('express');
+const projectController = require('../../controllers/project.controller');
 const {
   validateCreateProjects,
   validateGetProjectById,
   validateUpdateProject,
   validateGetProjectsByUserId,
-} = require("../../project.validator");
+} = require('../../project.validator');
 
 const projectRouter = express.Router();
 
 projectRouter.post(
-  "/projects",
+  '/projects',
   validateCreateProjects,
   projectController.createProjects
 );
 projectRouter.get(
-  "/projects/:userId",
+  '/projects/:userId',
   validateGetProjectsByUserId,
   projectController.getProjectsByUserId
 );
 projectRouter.get(
-  "/project/:id",
+  '/project/:id',
   validateGetProjectById,
   projectController.getProjectById
 );
 projectRouter.put(
-  "/project/:id",
+  '/project/:id',
   validateUpdateProject,
   projectController.updateProjectById
 );
